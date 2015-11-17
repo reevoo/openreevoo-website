caf69dd9219d472bf691d0f27e5dc5525e4290a3
Jonny Arnold <jonny.arnold@reevoo.com>
2015-11-17 09:42:37 +0000

    [+RB] Add project pages

--- web/router.ex
+++ web/router.ex
@@ -23,6 +23,7 @@ defmodule OpenreevooWebsite.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    get "/:project_name", PageController, :project
   end
 
   # Badge serving
